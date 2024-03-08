7338218160c2382df70fc899c949884513235f16
Dharamveer21 <139166346+Dharamveer21@users.noreply.github.com>
2024-03-08 10:25:33 +0000

    Update variable

--- Lectures/Lecture_4_Variables.js
+++ Lectures/Lecture_4_Variables.js
@@ -1,7 +1,7 @@
 const accountId = 144553;
 let accountEmail = "hitest@google.com";
 var accountPassword = "12345";
-accountCity = "Jaipur"; // by default it makes the data type let
+accountCity = "Jaipur"; // by default it makes the data type let but it is not a good practice
 
 let accountState;
 // accountId = 2 -- not allowed
@@ -17,5 +17,5 @@ Prefer not to use var
 because of issue in block scope & functional scope
 */
 
-console.table([accountId, accountEmail, accountPassword, accountCity,accountState]);
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
 
