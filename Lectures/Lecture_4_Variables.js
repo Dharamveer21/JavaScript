aba07ac99a4c5e8bf1ba339bee635a063777d81b
Dharamveer21 <139166346+Dharamveer21@users.noreply.github.com>
2024-03-08 09:48:08 +0000

    Lecture 4 Completed

---
+++ Lectures/Lecture_4_Variables.js
@@ -0,0 +1,21 @@
+const accountId = 144553;
+let accountEmail = "hitest@google.com";
+var accountPassword = "12345";
+accountCity = "Jaipur"; // by default it makes it let
+
+let accountState;
+// accountId = 2 -- not allowed
+
+accountEmail = "hc@hc.com";
+accountPassword = "21212121";
+accountCity = "Bengaluru";
+
+console.log(accountId);
+
+/*
+Prefer not to use var 
+because of issue in block scope & functional scope
+*/
+
+console.table([accountId, accountEmail, accountPassword, accountCity,accountState]);
+
